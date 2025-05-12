f0a38ed3943d6497298c5ef3659795f3287035dc
rose <rose.dangol23@gmail.com>
2025-05-12 18:44:09 +0545

    practice ig

---
+++ read.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+# img=cv.imread('images/cat.jpeg')
+
+# cv.imshow('cat',img)
+
+#read videos
+capture=cv.VideoCapture(0)
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video',frame)
+
+    if cv.waitKey(20)& 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
